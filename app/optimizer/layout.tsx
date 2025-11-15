740f0088fe3b8de9ca8751891c3886175722e2a3
Danny Kenneth <dannyk2008@hotmail.com>
2025-11-14 22:35:46 -0500

    Landing Page 2.0

--- app/optimizer/layout.tsx
+++ app/optimizer/layout.tsx
@@ -1,11 +1,8 @@
-const Layout = ({ children }: { children : React.ReactNode }) => {
+// /app/optimizer/layout.tsx
+export default function OptimizerLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center p-24">
-        <div className="container py-10">
-        {children}
-        </div>
+      <div className="container py-10">{children}</div>
     </main>
   )
 }
-
-export default Layout
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const Page = () => {
@@ -5,9 +7,13 @@ const Page = () => {
     <main>
       <div className='max-w-screen h-20 mx-auto border-white opacity-25 border-b-[0.25px]'>
       </div>
-        <div className="container bg-black text-white min-h-screen flex flex-col pl-80 pt-35">
+        <div className="container max-w-screen text-white min-h-screen flex flex-col pl-80 pt-35">
           <h1 className="text-3xl font-bold">Risk Optimizer</h1>
-          <h3 className='pt-3 font-extralight opacity-50'>The One Stop Solution for Risk<br / >and Portfolio Management</h3>
+          <h3 className='pt-3 font-normal opacity-50'>The One Stop Solution for Risk<br / >and Portfolio Management</h3>
+          <div className="flex h-5 pt-8">
+            <Button size="lg" className="mr-4 text-black-100 font-semibold"><Link href="/optimizer">Get Started</Link></Button>
+            <Button variant="outline" size="lg" className='border-0.5 border-white'>Learn More</Button>
+          </div>
         </div>
     </main>
   )
