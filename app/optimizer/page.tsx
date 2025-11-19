2defbb470341eadef917ec937f691b827c8fbbd8
Danny Kenneth <dannyk2008@hotmail.com>
2025-11-18 23:27:21 -0500

    Optimizer UI changeup

--- app/optimizer/page.tsx
+++ app/optimizer/page.tsx
@@ -1,3 +1,4 @@
+import { BentoGrid, BentoGridItem } from '@/components/ui/bento-grid'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
@@ -9,11 +10,9 @@ const Page = () => {
       <section className="min-h-screen justify-center bg-background">
         <div className='flex flex-col items-center gap-4'>
           <h1 className=" pt-24 justify-center text-white font-bold text-[36px]">Welcome!</h1>
-          <h3 className='text-white justify-center font-light'>Start by adding an asset.</h3>
-          <Button size="lg" className="mt-8 text-black-100 font-semibold bg-white">Add Asset</Button>
+          <h3 className='text-white justify-center font-light'>Type out your information, and receive a risk</h3>
+          <h3 className='text-white justify-center font-light'>score and optimization recomendations</h3>
           <div className="w-12 h-[0.25px] mt-4 bg-white"></div>
-          <h2 className="text-white opacity-30 text-[14px]">OR</h2>
-          <h1 className="font-semibold text-white justify-center text-[20px]">Choose a Template</h1>
         </div> 
         <div className="flex gap-2 m-48">/* Template Boxes Go Here */</div>       
       </section>
